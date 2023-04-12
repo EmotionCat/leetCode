5e126015201c4aa56f47fdd437e09b8de7ae8292
EmotionCat <xuechaowu2019@gmail.com>
2023-04-12 08:51:29 +0800

    feat: Passed question 766

--- Program.cs
+++ Program.cs
@@ -3,10 +3,7 @@
     private static void Main(string[] args)
     {
         Solution solution = new Solution();
-        int[][] image = new int[][] {new int[]{1,1,1}, new int[]{1,1,0}, new int[]{1,0,1}};
-        int sr = 1;
-        int sc = 1;
-        int color = 2;
-        Console.WriteLine(solution.FloodFill(image, sr, sc, color));
+        int[][] matrix = new int[][] {new int[]{1,2}, new int[]{2,2}};
+        Console.WriteLine(solution.IsToeplitzMatrix(matrix));
     }
 }
