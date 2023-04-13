1b60f0769545dd6fd188dbb9a08ec2d017d4e0a4
EmotionCat <xuechaowu2019@gmail.com>
2023-04-13 14:02:13 +0800

    feat:passed question 1534 and 1566

--- Program.cs
+++ Program.cs
@@ -3,8 +3,9 @@
     private static void Main(string[] args)
     {
         Solution solution = new Solution();
-        string num1 = "0";
-        string num2 = "0";
-        Console.WriteLine(solution.AddStrings(num1, num2));
+        int[] arr = {2,2,2,2};
+        int m = 2;
+        int k = 3;
+        Console.WriteLine(solution.ContainsPattern(arr,m, k));
     }
 }
--- Solution.cs
+++ Solution.cs
@@ -703,4 +703,77 @@ public class Solution
         str = new string(chars);
         return str;
     }
+
+    //1534.统计好三元组(简单，枚举)
+    // 给你一个整数数组 arr ，以及 a、b 、c 三个整数。请你统计其中好三元组的数量。
+    // 如果三元组 (arr[i], arr[j], arr[k]) 满足下列全部条件，则认为它是一个好三元组。
+    // 0 <= i < j < k < arr.length
+    // |arr[i] - arr[j]| <= a
+    // |arr[j] - arr[k]| <= b
+    // |arr[i] - arr[k]| <= c
+    // 其中 |x| 表示 x 的绝对值。
+    // 返回好三元组的数量。
+    // 示例1：输入：arr = [3,0,1,1,9,7], a = 7, b = 2, c = 3，输出：4
+    // 解释：一共有 4 个好三元组：[(3,0,1), (3,0,1), (3,1,1), (0,1,1)]。
+    // 示例2：输入：arr = [1,1,2,2,3], a = 0, b = 0, c = 1，输出：0
+    // 解释：不存在满足所有条件的三元组。
+    public int CountGoodTriplets(int[] arr, int a, int b, int c)
+    {
+        int count = 0;
+        for (int i = 0; i < arr.Length - 2; i++)
+        {
+            for (int j = i + 1; j < arr.Length - 1; j++)
+            {
+                for (int k = j + 1; k < arr.Length; k++)
+                {
+                    if (Math.Abs(arr[i] - arr[k]) <= c)
+                    {
+                        if (Math.Abs(arr[i] - arr[j]) <= a)
+                        {
+                            if (Math.Abs(arr[j] - arr[k]) <= b) count++;
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    //1566.重复至少 K 次且长度为 M 的模式(简单，枚举)
+    // 给你一个正整数数组 arr，请你找出一个长度为 m 且在数组中至少重复 k 次的模式。
+    // 模式是由一个或多个值组成的子数组（连续的子序列），连续重复多次但不重叠。 模式由其长度和重复次数定义。
+    // 如果数组中存在至少重复 k 次且长度为 m 的模式，则返回 true ，否则返回  false 。
+    // 示例1：输入：arr = [1,2,4,4,4,4], m = 1, k = 3，输出：true
+    // 解释：模式 (4) 的长度为 1 ，且连续重复 4 次。注意，模式可以重复 k 次或更多次，但不能少于 k 次。
+    // 示例2：输入：arr = [1,2,1,2,1,1,1,3], m = 2, k = 2，输出：true
+    // 解释：模式 (1,2) 长度为 2 ，且连续重复 2 次。另一个符合题意的模式是 (2,1) ，同样重复 2 次。
+    // 示例3：输入：arr = [1,2,1,2,1,3], m = 2, k = 3，输出：false
+    // 解释：模式 (1,2) 长度为 2 ，但是只连续重复 2 次。不存在长度为 2 且至少重复 3 次的模式。
+    // 示例4：输入：arr = [1,2,3,1,2], m = 2, k = 2，输出：false
+    // 解释：模式 (1,2) 出现 2 次但并不连续，所以不能算作连续重复 2 次。
+    // 示例5：输入：arr = [2,2,2,2], m = 2, k = 3，输出：false
+    // 解释：长度为 2 的模式只有 (2,2) ，但是只连续重复 2 次。注意，不能计算重叠的重复次数。
+    public bool ContainsPattern(int[] arr, int m, int k)
+    {
+        if (m * k > arr.Length) return false;
+        int flagK = 0;
+        for (int i = 0; i < arr.Length - m * k + 1; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                for (int l = 1; l < k; l++)
+                {
+                    if (arr[i + j] != arr[i + j + l * m])
+                    {
+                        flagK = -1;
+                        break;
+                    }
+                }
+                if(flagK == -1) break;
+            }
+            if (flagK != -1) return true;
+            flagK = 0;
+        }
+        return false;
+    }
 }
