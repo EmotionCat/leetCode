65eb77e30c59674b3eb570a6e7efe853d4c695dd
EmotionCat <xuechaowu2019@gmail.com>
2023-04-18 17:40:57 +0800

    feat:passed question 11

--- Program.cs
+++ Program.cs
@@ -3,8 +3,7 @@
     private static void Main(string[] args)
     {
         Solution solution = new Solution();
-        int[] arr1 = new int[] {28,6,22,8,44,17};
-        int[] arr2 = new int[] {22,28,8,6};
-        Console.WriteLine(solution.RelativeSortArray(arr1, arr2));
+        int[] height = new int[] {1,8,6,2,5,4,8,3,7};
+        Console.WriteLine(solution.MaxArea(height));
     }
 }
--- Solution.cs
+++ Solution.cs
@@ -1165,5 +1165,44 @@ public class Solution
         return arr1;    
     }
 
-    
+    // 11. 盛最多水的容器(中等, 数组)
+    // 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+    // 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+    // 返回容器可以储存的最大水量。
+    // 说明：你不能倾斜容器。
+    // 示例1：
+    // 输入：[1,8,6,2,5,4,8,3,7]
+    // 输出：49 
+    // 解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+    // 示例 2：
+    // 输入：height = [1,1]
+    // 输出：1
+    public int MaxArea(int[] height)
+    {
+        /* 可以计算出结果，但是当height数据过长时，超出时间限制
+        int maxArea = 0;
+        for (int i = 0; i < height.Length - 1; i++)
+        {
+            for (int j = i + 1; j < height.Length; j++)
+            {
+                int area = (height[i] < height[j] ? height[i] : height[j]) * (j - i);
+                if (area > maxArea) maxArea = area;
+            }
+        }
+        return maxArea;
+        */
+        // 双指针！！！！！！！
+        int maxArea = 0;
+        int i = 0;
+        int j = height.Length - 1;
+        while (i < j)
+        {
+            int area = (height[i] < height[j] ? height[i] : height[j]) * (j - i);
+            if (area > maxArea) maxArea = area;
+            if (height[i] < height[j]) i++;
+            else j--;
+        }
+        return maxArea;
+
+    }
 }
